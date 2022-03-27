56317fe255f12f18036a3ccdbafd951fec5b5484
Aaron AN <acangle10@gmail.com>
2022-03-26 22:04:00 -0700

    stuff

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <p>{counter}</p>
+        <button onClick={() => setCounter(c => c + 1)}>Increment</button>
       </header>
     </div>
   );
