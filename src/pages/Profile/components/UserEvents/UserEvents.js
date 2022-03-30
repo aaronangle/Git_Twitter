e09b44866a5ca04fe66b7e2af21bd2618a078134
aaron.angle <aaron.angle@IT-ANGLE>
2022-03-30 15:20:41 -0700

    events on profile

--- src/pages/Profile/components/UserEvents/UserEvents.js
+++ src/pages/Profile/components/UserEvents/UserEvents.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { RowContainer } from 'components/RowContainer';
+import { EventRow } from 'components/EventRow';
 
-export const UserEvents = () => {
-  return <></>;
+export const UserEvents = ({ events }) => {
+  return (
+    <>
+      {events.map((event) => {
+        return (
+          <RowContainer>
+            <EventRow event={event} />
+          </RowContainer>
+        );
+      })}
+    </>
+  );
 };
